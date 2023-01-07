475530dbcd14b65bba050d5db0c7bb2923618022
Vladimir S <rivalfpv@gmail.com>
2023-01-07 23:42:52 +0500

    tests paths update

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 import pathbuster.pathbuster as pathbuster
-
+from pathbuster.classes.response import Response
 
 required_args = [
     '-u', './test/testurls',
@@ -11,14 +11,14 @@ def test_filter_regex():
     pathbuster.conf.filter_regex = '<title>Admin'
     pathbuster.conf.exclude_codes = [301,]
 
-    res1 = pathbuster.RequestResult(url='http://example.com/test', status=200, reason='OK', body=b'<title>Member</title> etc', headers=[], parent_url=None)
+    res1 = Response(url='http://example.com/test', status=200, reason='OK', body=b'<title>Member</title> etc', headers=[], parent_url=None)
     assert(pathbuster.result_valid(res1) == False)
 
-    res2 = pathbuster.RequestResult(url='http://example.com/test', status=404, reason='OK', body=b'bla \n <title>Admin Panel</title> ', headers=[], parent_url=None)
+    res2 = Response(url='http://example.com/test', status=404, reason='OK', body=b'bla \n <title>Admin Panel</title> ', headers=[], parent_url=None)
     assert(pathbuster.result_valid(res2) == True)
 
     #code is filtered
-    res2 = pathbuster.RequestResult(url='http://example.com/test', status=301, reason='OK', body=b'bla \n <title>Admin Panel</title> ', headers=[], parent_url=None)
+    res2 = Response(url='http://example.com/test', status=301, reason='OK', body=b'bla \n <title>Admin Panel</title> ', headers=[], parent_url=None)
     assert(pathbuster.result_valid(res2) == False)
 
 
@@ -28,23 +28,23 @@ def test_not_ac():
     args.extend(['-e', '401,404,400'])
     pathbuster.parse_args(args)
 
-    res1 = pathbuster.RequestResult(url='http://example.com/test', status=200, reason='OK', body=b'', headers=[], parent_url=None)
+    res1 = Response(url='http://example.com/test', status=200, reason='OK', body=b'', headers=[], parent_url=None)
     assert(pathbuster.result_valid(res1) == True)
 
-    res2 = pathbuster.RequestResult(url='http://example.com/test', status=400, reason='NOT OK', body=b'', headers=[], parent_url=None)
+    res2 = Response(url='http://example.com/test', status=400, reason='NOT OK', body=b'', headers=[], parent_url=None)
     assert(pathbuster.result_valid(res2) == False)
 
-    res3 = pathbuster.RequestResult(url='http://example.com/test', status=401, reason='NOT OK', body=b'', headers=[], parent_url=None)
+    res3 = Response(url='http://example.com/test', status=401, reason='NOT OK', body=b'', headers=[], parent_url=None)
     assert(pathbuster.result_valid(res3) == False)
 
 
 def test_empty_e():
     pathbuster.conf.exclude_codes = []
     
-    res1 = pathbuster.RequestResult(url='http://example.com/test', status=200, reason='OK', body=b'', headers=[], parent_url=None)
+    res1 = Response(url='http://example.com/test', status=200, reason='OK', body=b'', headers=[], parent_url=None)
     assert(pathbuster.result_valid(res1) == True)
 
-    res2 = pathbuster.RequestResult(url='http://example.com/test', status=400, reason='NOT OK', body=b'', headers=[], parent_url=None)
+    res2 = Response(url='http://example.com/test', status=400, reason='NOT OK', body=b'', headers=[], parent_url=None)
     assert(pathbuster.result_valid(res2) == True)
 
 
@@ -52,16 +52,16 @@ def test_ac():
     args = required_args.copy()
     args.extend(['-ac'])
     pathbuster.parse_args(args)
-    pf_res1 = pathbuster.RequestResult(url='http://example.com/test', status=200, reason='OK', body=b'1 2 3\n4 5', headers=[], parent_url='http://example.com')
-    pf_res2 = pathbuster.RequestResult(url='http://example.com/redirect', status=301, reason='OK', body=b'1 2 3\n4 5\n6 7', headers=[], parent_url='http://example.com')
+    pf_res1 = Response(url='http://example.com/test', status=200, reason='OK', body=b'1 2 3\n4 5', headers=[], parent_url='http://example.com')
+    pf_res2 = Response(url='http://example.com/redirect', status=301, reason='OK', body=b'1 2 3\n4 5\n6 7', headers=[], parent_url='http://example.com')
     pathbuster.preflight_samples = {
         'http://example.com': [pf_res1, pf_res2],
     }
     assert(pathbuster.result_valid(pf_res1) == False)
     assert(pathbuster.result_valid(pf_res2) == False)
-    res = pathbuster.RequestResult(url='http://example.com/test2', status=200, reason='OK', body=b'1 2 3\n4 5 6', headers=[], parent_url='http://example.com')
+    res = Response(url='http://example.com/test2', status=200, reason='OK', body=b'1 2 3\n4 5 6', headers=[], parent_url='http://example.com')
     assert(pathbuster.result_valid(res) == True)
-    res2 = pathbuster.RequestResult(url='http://example.com/test22', status=301, reason='OK', body=b'1 2 3\n4 5 6', headers=[], parent_url='http://example.com')
+    res2 = Response(url='http://example.com/test22', status=301, reason='OK', body=b'1 2 3\n4 5 6', headers=[], parent_url='http://example.com')
     assert(pathbuster.result_valid(res2) == True)
-    res3 = pathbuster.RequestResult(url='http://example2.com/test222', status=200, reason='OK', body=b'1 2 3\n4 5 6', headers=[], parent_url='http://example2.com')
+    res3 = Response(url='http://example2.com/test222', status=200, reason='OK', body=b'1 2 3\n4 5 6', headers=[], parent_url='http://example2.com')
     assert(pathbuster.result_valid(res3) == True)
--- tests/test_req_res.py
+++ tests/test_req_res.py
@@ -1,11 +1,11 @@
-from pathbuster.pathbuster import RequestResult
+from pathbuster.pathbuster import Response
 
 
 def test_init():
     bodyb = b'one two\nthree four'
     headers = {'Cookie': 'test=1234;'}
     parent_url = 'http://example.com'
-    req = RequestResult('http://example.com/admin', 200, 'OK', bodyb, headers, parent_url, 'meta1')
+    req = Response('http://example.com/admin', 200, 'OK', bodyb, headers, parent_url, 'meta1')
     assert(len(req.headers) == 1)
     assert(req.headers['Cookie'] == 'test=1234;')
     assert(req.strbody == 'one two\nthree four')
